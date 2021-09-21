2f9e98171e4afe6a000baf90046b47f293b0cf22
pate1798 <pate1798@sheridancollege.ca>
2021-09-20 23:02:07 -0400

    sprint 3i

--- Controllers/CustomShoesController.cs
+++ Controllers/CustomShoesController.cs
@@ -1,9 +1,11 @@
 ﻿
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using TapShoesCanada.Data;
 using TapShoesCanada.Models;
@@ -27,13 +29,29 @@ namespace TapShoesCanada.Controllers
 
         public IActionResult Form()
         {
+
+
+
+            string[] smodel = new string[] { "Jazz Tap Master", "Triple Threat", "Tap Boot", "Rhythm HeelsPro", "Broadway Diva", "Classic Jane", "New Show Girl", "T Step", "La Coquette", "Sportap" };
+            SelectList list1 = new SelectList(smodel);
+            ViewBag.Smodel = list1;
+
+            string[] sole = new string[] { "Black", "Natural", "Wood" };
+            SelectList list2 = new SelectList(sole);
+            ViewBag.sole = list2;
+
+
+            string[] Style = new string[] { "Regular", "Royal", "Eyelets" }; // for different syles of a shoe
+           
+            SelectList list = new SelectList(Style);
+            ViewBag.Style = list;
             return View();
 
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Size,Style,Model,Colour1,Colour2,Sole,Lace")] Custom_shoe cshoe)
+        public async Task<IActionResult> Create([Bind("Size,Style,Model,Colour1,Colour2,Sole,Lace")] Custom_shoe cshoe)
         {
             if (ModelState.IsValid)
             {
